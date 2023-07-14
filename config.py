31bf299e3825a22d8c6f441bb61ac8b07f396e56
pujapardeshi <puja.pardeshi47@gmail.com>
2023-07-14 15:49:45 +0530

    committed

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+PORT_NUMBER = 8080
+
+MONGODB_PORT_NUMBER = 27017
+MONGODB_DATABASE_NAME = 'project_db'
+
+MODEL_FILE_PATH = r'E:\Python Projects\Project\proj_data\linear_regression.pkl'
+
+JSON_FILE_PATH = r'E:\Python Projects\Project\proj_data\proj_data.json'
